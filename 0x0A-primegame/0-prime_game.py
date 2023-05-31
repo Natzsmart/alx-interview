ce860c20c7084a4ba7bf9d614a10e6eaedfa2e7e
Smartino <Iniobongeffiong40@gmail.com>
2023-05-31 10:16:23 -0700

    code changed

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
-""" Prime Game  """
+"""Prime game module.
+"""
 
 
 def isWinner(x, nums):
-    """ Returns the winner of the Prime Game """
-    if x == 0 or x == -1:
+    """Determines the winner of a prime game session with `x` rounds.
+    """
+    if x < 1 or not nums:
         return None
-    elif x == 10000:
-        return "Maria"
-    return "Ben"
+    marias_wins, bens_wins = 0, 0
+    # generate primes with a limit of the maximum number in nums
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i + i, n + 1, i):
+            primes[j - 1] = False
+    # filter the number of primes less than n in nums for each round
+    for _, n in zip(range(x), nums):
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        bens_wins += primes_count % 2 == 0
+        marias_wins += primes_count % 2 == 1
+    if marias_wins == bens_wins:
+        return None
+    return 'Maria' if marias_wins > bens_wins else 'Ben'
